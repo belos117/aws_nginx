0e936a0dd7471f97e62ae5ad2df4defbd259390d
stormhenkel <belos117@gmail.com>
2024-01-27 20:59:53 +0100

    Updated some files

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-resource "aws_security_group" "allow_tls" {
-  name = var.security_group_name
-
-  dynamic "ingress" {
-    for_each = var.ingress_rule
-
-    content {
-      from_port   = ingress.value.from_port
-      to_port     = ingress.value.to_port
-      protocol    = ingress.value.protocol
-      cidr_blocks = ingress.value.cidr_blocks
-    }
-  }
-
-  tags = {
-    Name = var.security_group_tag
-  }
-}
 resource "aws_instance" "nginx_server" {
   ami           = var.ami
   instance_type = var.instance_type
@@ -46,4 +28,21 @@ EOF
 
 }
 
+resource "aws_security_group" "allow_tls" {
+  name = var.security_group_name
 
+  dynamic "ingress" {
+    for_each = var.ingress_rule
+
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+
+  tags = {
+    Name = var.security_group_tag
+  }
+}
--- output.tf
+++ output.tf
@@ -5,5 +5,5 @@ output "instances" {
 
 output "public_ip" {
   description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
-  value = aws_instance.nginx_server.*.public_ip
+  value       = aws_instance.nginx_server.*.public_ip
 }
