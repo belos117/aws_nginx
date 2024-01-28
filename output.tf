9545fcfca2b38233761ba217a03b47b2466f65f5
stormhenkel <belos117@gmail.com>
2024-01-28 12:20:33 +0100

    Modified ouput.tf file

--- output.tf
+++ output.tf
@@ -4,6 +4,6 @@ output "instances" {
 }
 
 output "public_ip" {
-  description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
+  description = "The public IP address assigned to the instance"
   value       = aws_instance.nginx_server.*.public_ip
 }
